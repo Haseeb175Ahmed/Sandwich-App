1e5d2591dbb1e4b7ca3e9e1a1d573d480233d473
Dell <Dell@DESKTOP-HKCRJCS>
2022-02-16 00:42:07 +0500

    Add project files.

---
+++ Sandwich App/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace Sandwich_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fromDate = new System.Windows.Forms.DateTimePicker();
+            this.toDate = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.LeaveswithSandwich = new System.Windows.Forms.Label();
+            this.LeaveswithoutSandwich = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(43, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "From Date";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(43, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "To Date";
+            // 
+            // fromDate
+            // 
+            this.fromDate.CustomFormat = "dd/MMM/yyyy";
+            this.fromDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.fromDate.Location = new System.Drawing.Point(187, 29);
+            this.fromDate.Name = "fromDate";
+            this.fromDate.Size = new System.Drawing.Size(133, 20);
+            this.fromDate.TabIndex = 2;
+            // 
+            // toDate
+            // 
+            this.toDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.toDate.Location = new System.Drawing.Point(187, 66);
+            this.toDate.Name = "toDate";
+            this.toDate.Size = new System.Drawing.Size(133, 20);
+            this.toDate.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(208, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // LeaveswithSandwich
+            // 
+            this.LeaveswithSandwich.AutoSize = true;
+            this.LeaveswithSandwich.Location = new System.Drawing.Point(297, 203);
+            this.LeaveswithSandwich.Name = "LeaveswithSandwich";
+            this.LeaveswithSandwich.Size = new System.Drawing.Size(35, 13);
+            this.LeaveswithSandwich.TabIndex = 5;
+            this.LeaveswithSandwich.Text = "label3";
+            // 
+            // LeaveswithoutSandwich
+            // 
+            this.LeaveswithoutSandwich.AutoSize = true;
+            this.LeaveswithoutSandwich.Location = new System.Drawing.Point(297, 159);
+            this.LeaveswithoutSandwich.Name = "LeaveswithoutSandwich";
+            this.LeaveswithoutSandwich.Size = new System.Drawing.Size(35, 13);
+            this.LeaveswithoutSandwich.TabIndex = 6;
+            this.LeaveswithoutSandwich.Text = "label4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(93, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(176, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Leaves With Out Sandwich";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(93, 203);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(150, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Leaves With Sandwich";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 270);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.LeaveswithoutSandwich);
+            this.Controls.Add(this.LeaveswithSandwich);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.toDate);
+            this.Controls.Add(this.fromDate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker fromDate;
+        private System.Windows.Forms.DateTimePicker toDate;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label LeaveswithSandwich;
+        private System.Windows.Forms.Label LeaveswithoutSandwich;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Sandwich App/Form1.cs
@@ -0,0 +1,105 @@
+﻿using MFilesAPI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sandwich_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime FromDate = Convert.ToDateTime(fromDate.Text);
+            DateTime ToDate = Convert.ToDateTime(toDate.Text);
+
+            var listDates = GetGazettedHolidays();
+
+            int SandwichLeaves = 0;
+            int Leaves = 0;
+
+
+            for (DateTime date = FromDate; ToDate.CompareTo(date) >= 0; date = date.AddDays(1.0))
+            {
+                if (listDates.Contains(date) || date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    SandwichLeaves++;
+                }
+                else
+                {
+                    Leaves++;
+                }
+            }
+
+            LeaveswithSandwich.Text = (SandwichLeaves + Leaves).ToString();
+            LeaveswithoutSandwich.Text = Leaves.ToString();
+
+        }
+
+
+
+        public static List<DateTime> GetGazettedHolidays()
+        {
+            List<DateTime> GazettedHolidays = new List<DateTime>();
+
+            try
+            {
+                DateTime KashmirDay = new DateTime(2022, 02, 05);
+                DateTime PakistanDay = new DateTime(2022, 03, 23);
+                DateTime LabourDay = new DateTime(2022, 05, 01);
+                DateTime Eid_ul_Fitr_Starts = new DateTime(2022, 05, 03);
+                DateTime Eid_ul_Fitr_Ends = new DateTime(2022, 05, 05);
+                DateTime Eid_ul_Azha_Starts = new DateTime(2022, 07, 10);
+                DateTime Eid_ul_Azha_Ends = new DateTime(2022, 07, 12);
+                DateTime Ashura_9th = new DateTime(2022, 08, 07);
+                DateTime Ashura_10th = new DateTime(2022, 08, 08);
+                DateTime IndependenceDay = new DateTime(2022, 08, 14);
+                DateTime Eid_Milad_un_Nabi = new DateTime(2022, 10, 09);
+                DateTime Quaid_e_Azam_Day_Christmas = new DateTime(2022, 12, 25);
+
+
+                GazettedHolidays.Add(KashmirDay);
+                GazettedHolidays.Add(PakistanDay);
+                GazettedHolidays.Add(LabourDay);
+                for (DateTime date = Eid_ul_Fitr_Starts; Eid_ul_Fitr_Ends.CompareTo(date) >= 0; date = date.AddDays(1.0))
+                {
+                    GazettedHolidays.Add(date);
+                }
+
+                for (DateTime date1 = Eid_ul_Azha_Starts; Eid_ul_Azha_Ends.CompareTo(date1) >= 0; date1 = date1.AddDays(1.0))
+                {
+                    GazettedHolidays.Add(date1);
+                }
+
+                GazettedHolidays.Add(Ashura_9th);
+                GazettedHolidays.Add(Ashura_10th);
+                GazettedHolidays.Add(IndependenceDay);
+                GazettedHolidays.Add(Eid_Milad_un_Nabi);
+                GazettedHolidays.Add(Quaid_e_Azam_Day_Christmas);
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+            return GazettedHolidays;
+        }
+    }
+}
